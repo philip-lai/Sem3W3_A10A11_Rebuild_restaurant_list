638d2f6c8b845d3ffa6692111076b8d0105b6c8e
philip_lai <pupuhouse@yahoo.com.tw>
2019-11-16 19:05:28 +0800

    add detail page

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// 引用 body-parser
+const bodyParser = require('body-parser');
+// 設定 bodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // setting static files
 app.use(express.static('public'))
 
@@ -46,26 +51,55 @@ app.get('/', (req, res) => {
 app.get('/restaurants', (req, res) => {
   res.send('列出所有 Restaurant')
 })
+
 // 新增一筆 Todo 頁面
 app.get('/restaurants/new', (req, res) => {
-  res.send('新增 Restaurant 頁面')
+  return res.render('new')
+  // res.send('新增 Restaurant 頁面')
 })
+
 // 顯示一筆 Todo 的詳細內容
 app.get('/restaurants/:id', (req, res) => {
-  res.send('顯示 Restaurant 的詳細內容')
+  Restaurant.findById(req.params.id, (err, restaurant) => {
+    if (err) return console.error(err)
+    return res.render('detail', { restaurant: restaurant })
+  })
+  // res.send('顯示 Restaurant 的詳細內容')
 })
+
 // 新增一筆  Todo
 app.post('/restaurants', (req, res) => {
-  res.send('建立 Restaurnat')
+  // 建立Todo model 實例
+  const restaurant = new Restaurant({
+    name: req.body.name,
+    name_en: req.body.name_en,
+    category: req.body.category,
+    location: req.body.location,
+    phone: req.body.phone,
+    description: req.body.description,
+    google_map: req.body.google_map,
+    image: req.body.image,
+    rating: req.body.rating
+  })
+
+  // 存入資料庫
+  restaurant.save(err => {
+    if (err) return console.error(err)
+    return res.redirect('/')
+  })
+  // res.send('建立 Restaurnat')
 })
+
 // 修改 Todo 頁面
 app.get('/restaurants/:id/edit', (req, res) => {
   res.send('修改 Restaurant 頁面')
 })
+
 // 修改 Todo
 app.post('/restaurants/:id/edit', (req, res) => {
   res.send('修改 Restaurant')
 })
+
 // 刪除 Todo
 app.post('/restaurants/:id/delete', (req, res) => {
   res.send('刪除 Restaurant')
